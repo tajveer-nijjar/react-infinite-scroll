a058a41bc3c558a4fc18f58801b6c86e8f87f180
tajveer singh nijjar <taj_nijjar2002@yahoo.co.in>
2018-11-12 11:08:54 +1300

    Working infinite scroll.

--- src/features/contact-list/index.js
+++ src/features/contact-list/index.js
@@ -4,34 +4,75 @@ import Contact from "./contact";
 
 class ContactList extends React.Component {
   state = {
-    contacts: []
+    contacts: [],
+    per: 8,
+    page: 1,
+    totalPages: null,
+    scrolling: false
   };
 
   componentWillMount() {
     this.loadContacts();
+    this.scrollListerer = window.addEventListener("scroll", e => {
+      this.handleScroll(e);
+    });
   }
 
+  handleScroll = e => {
+    const { scrolling, totalPages, page } = this.state;
+
+    if (scrolling) return;
+
+    if (totalPages <= page) return;
+
+    const lastLi = document.querySelector("ol.contacts > li:last-Child");
+
+    const lastLiOffset = lastLi.offsetTop + lastLi.clientHeight;
+    const pageOffset = window.pageYOffset + window.innerHeight;
+    var bottomOffset = 50;
+    if (pageOffset > lastLiOffset - bottomOffset) {
+      this.loadMore();
+    }
+  };
+
   loadContacts = () => {
-    fetch("https://student-example-api.herokuapp.com/v1/contacts.json")
+    const { per, page, contacts } = this.state;
+    const url = `https://student-example-api.herokuapp.com/v1/contacts.json?per=${per}&page=${page}`;
+
+    fetch(url)
       .then(response => response.json())
       .then(json =>
         this.setState({
-          contacts: json.contacts
+          contacts: [...contacts, ...json.contacts],
+          scrolling: false,
+          totalPages: json.total_pages
         })
       );
   };
 
+  loadMore = () => {
+    this.setState(
+      prevState => ({
+        page: prevState.page + 1,
+        scrolling: true
+      }),
+      this.loadContacts
+    );
+  };
+
   render() {
     return (
-      <ul className="contacts">
-        {this.state.contacts.map(contact => {
-          return (
-            <li>
-              <Contact {...contact} />
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        <ol className="contacts">
+          {this.state.contacts.map(contact => {
+            return (
+              <li key={contact.id}>
+                <Contact {...contact} />
+              </li>
+            );
+          })}
+        </ol>
+      </div>
     );
   }
 }
